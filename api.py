273dc269a861cfa7bc9fb29e339166c3b55cfe23
NajaAFonseca <najafonseca777@gmail.com>
2023-02-10 08:36:59 -0800

    s

--- api.py
+++ api.py
@@ -8,6 +8,13 @@ Activities = {
 
 }
 
+class Activities(Resource):
+    def get(self):
+    def post(self):
+
+
+api.add_resource(ActivitiesList, '/activities/')
+
 if __name__ == '__main__': {
     app.run(debug==True)
 }
