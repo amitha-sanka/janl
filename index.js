2380bbf331f6c4bd7d98a9b75eb9f31e1f3b0c19
NajaAFonseca <najafonseca777@gmail.com>
2023-02-09 22:04:39 -0800

    del node

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('express')();
-const PORT = 8086;
-
-app.get('/activity', (req, res) => {
-    res.status(200).send({
-        activity: 'SeaWorld',
-        hobby: 'amusement park'
-    })
-});
