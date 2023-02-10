968106f0076401216b1bc877ca684f1dbed2dbed
NajaAFonseca <najafonseca777@gmail.com>
2023-02-10 12:09:48 -0800

    s

--- api/api.py
+++ api/api.py
@@ -30,10 +30,38 @@ class ActivitiesList(Resource):
             "duration": args["duration"] 
         }
 
-        return ACTIVITIES[activity_id], 201
+        return ACTIVITIES[activity_id], 200
 
+
+class Activities(Resource):
+    def get(self, activity_id):
+        if activity_id not in ACTIVITIES:
+            return 'Not Found', 404
+        else: 
+            return ACTIVITIES[activity_id]
+
+    def put(self, activity_id):
+        parser.add_argument("activity")
+        parser.add_argument("hobby")
+        parser.add_argument("price_range")
+        parser.add_argument("duration")
+        args = parser.parse_args()
+
+        if activity_id not in ACTIVITIES:
+            return 'Record Not Found', 404
+        else: 
+            activity = ACTIVITIES[activity_id]
+            activity["activity"] = args["activity"] if args["activity"] is not None else activity ["activity"]
+            activity["hobby"] = args["hobby"] if args["hobby"] is not None else activity ["hobby"]
+            activity["price_range"] = args["price_range"] if args["price_range"] is not None else activity ["price_range"]
+            activity["duration"] = args["duration"] if args["duration"] is not None else activity ["duration"]
+            return activity, 200
+
+
+    def delete(self, activity_id):
 
 api.add_resource(ActivitiesList, '/api/')
+api.add_resource(Activity, '/api/<activity_id>')
 
 if __name__ == '__main__': {
     app.run(debug==True)
